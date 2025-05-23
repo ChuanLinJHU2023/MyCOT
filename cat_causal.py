6acf292e9885ce542719a04d210a9b9a2085f983
Chuan Lin 2025 <1304404717@qq.com>
2025-05-23 04:09:55 -0400

    Second Submission

--- cat_causal.py
+++ cat_causal.py
@@ -4,6 +4,7 @@ from distance_causal_transport import calculate_causal_distance
 from utils import *
 
 down_factor = 32
+C=4
 img = plt.imread('cat_images/A.jpg')
 img_A = np.array(img)
 img_A = downsample_image(img_A, down_factor)
@@ -19,18 +20,18 @@ img_D = downsample_image(img_D, down_factor)
 
 
 print("################### Causal Distance Between AA #########################")
-dist_AA, _ = calculate_causal_distance_between_images(img_A, img_A)
+dist_AA, _ = calculate_causal_distance_between_images(img_A, img_A, scaling_parameter_c=C)
 print(dist_AA)
 print("#################################################################")
 
 
 print("################### Causal Distance Between AB #########################")
-dist_AB, _ = calculate_causal_distance_between_images(img_A, img_B)
+dist_AB, _ = calculate_causal_distance_between_images(img_A, img_B, scaling_parameter_c=C)
 print(dist_AB)
 print("#################################################################")
 
 
-print("################### Causal Distance Between AA #########################")
-dist_AD, _ = calculate_causal_distance_between_images(img_A, img_D)
+print("################### Causal Distance Between AD #########################")
+dist_AD, _ = calculate_causal_distance_between_images(img_A, img_D, scaling_parameter_c=C)
 print(dist_AD)
 print("#################################################################")
--- cat_wasserstein.py
+++ cat_wasserstein.py
@@ -3,9 +3,12 @@ from distance_wasserstein import *
 from utils import *
 
 down_factor = 32
+C=4
 img = plt.imread('cat_images/A.jpg')
 img_A = np.array(img)
 img_A = downsample_image(img_A, down_factor)
+print("This is our shape!!!!!")
+print(img_A.shape)
 
 img = plt.imread('cat_images/B.jpg')
 img_B = np.array(img)
@@ -18,18 +21,18 @@ img_D = downsample_image(img_D, down_factor)
 
 
 print("################### Wasser Distance Between AA #########################")
-dist_AA, _ = calculate_wasserstein_distance_between_images(img_A, img_A)
+dist_AA, _ = calculate_wasserstein_distance_between_images(img_A, img_A, scaling_parameter_c=C)
 print(dist_AA)
 print("#################################################################")
 
 
 print("################### Wasser Distance Between AB #########################")
-dist_AB, _ = calculate_wasserstein_distance_between_images(img_A, img_B)
+dist_AB, _ = calculate_wasserstein_distance_between_images(img_A, img_B, scaling_parameter_c=C)
 print(dist_AB)
 print("#################################################################")
 
 
-print("################### Wasser Distance Between AA #########################")
-dist_AD, _ = calculate_wasserstein_distance_between_images(img_A, img_D)
+print("################### Wasser Distance Between AD #########################")
+dist_AD, _ = calculate_wasserstein_distance_between_images(img_A, img_D, scaling_parameter_c=C)
 print(dist_AD)
 print("#################################################################")
