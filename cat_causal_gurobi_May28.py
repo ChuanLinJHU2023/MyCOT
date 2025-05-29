79526cab94aa8bd950269c35c2fa74ef1f5262c6
Chuan Lin 2025 <1304404717@qq.com>
2025-05-28 20:41:42 -0400

    May28 - 7

--- cat_causal_gurobi_May28.py
+++ cat_causal_gurobi_May28.py
@@ -33,17 +33,17 @@ print(dist_AC)
 print_format_string("DISTANCE AD", 20)
 dist_AD, _ = calculate_causal_distance_between_images(img_A, img_D, scaling_parameter_c)
 print(dist_AD)
-
-print_format_string("DISTANCE BA", 20)
-dist_BA, _ = calculate_causal_distance_between_images(img_B, img_A, scaling_parameter_c)
-print(dist_BA)
-
-print_format_string("DISTANCE CA", 20)
-dist_CA, _ = calculate_causal_distance_between_images(img_C, img_A, scaling_parameter_c)
-print(dist_CA)
-
-
-print_format_string("DISTANCE DA", 20)
-dist_DA, _ = calculate_causal_distance_between_images(img_D, img_A, scaling_parameter_c)
-print(dist_DA)
-
+#
+# print_format_string("DISTANCE BA", 20)
+# dist_BA, _ = calculate_causal_distance_between_images(img_B, img_A, scaling_parameter_c)
+# print(dist_BA)
+#
+# print_format_string("DISTANCE CA", 20)
+# dist_CA, _ = calculate_causal_distance_between_images(img_C, img_A, scaling_parameter_c)
+# print(dist_CA)
+#
+#
+# print_format_string("DISTANCE DA", 20)
+# dist_DA, _ = calculate_causal_distance_between_images(img_D, img_A, scaling_parameter_c)
+# print(dist_DA)
+#
--- cat_causal_pulp_May28.py
+++ cat_causal_pulp_May28.py
@@ -21,28 +21,28 @@ print(img_A.shape)
 print_format_string("SCALING FACTOR", 20)
 print(scaling_parameter_c)
 
-print_format_string("DISTANCE AB", 20)
-dist_AB, _ = calculate_causal_distance_between_images(img_A, img_B, scaling_parameter_c)
-print(dist_AB)
-
-print_format_string("DISTANCE AC", 20)
-dist_AC, _ = calculate_causal_distance_between_images(img_A, img_C, scaling_parameter_c)
-print(dist_AC)
+# print_format_string("DISTANCE AB", 20)
+# dist_AB, _ = calculate_causal_distance_between_images(img_A, img_B, scaling_parameter_c)
+# print(dist_AB)
+#
+# print_format_string("DISTANCE AC", 20)
+# dist_AC, _ = calculate_causal_distance_between_images(img_A, img_C, scaling_parameter_c)
+# print(dist_AC)
 
 print_format_string("DISTANCE AD", 20)
 dist_AD, _ = calculate_causal_distance_between_images(img_A, img_D, scaling_parameter_c)
 print(dist_AD)
 
-print_format_string("DISTANCE BA", 20)
-dist_BA, _ = calculate_causal_distance_between_images(img_B, img_A, scaling_parameter_c)
-print(dist_BA)
-
-print_format_string("DISTANCE CA", 20)
-dist_CA, _ = calculate_causal_distance_between_images(img_C, img_A, scaling_parameter_c)
-print(dist_CA)
-
-
-print_format_string("DISTANCE DA", 20)
-dist_DA, _ = calculate_causal_distance_between_images(img_D, img_A, scaling_parameter_c)
-print(dist_DA)
-
+# print_format_string("DISTANCE BA", 20)
+# dist_BA, _ = calculate_causal_distance_between_images(img_B, img_A, scaling_parameter_c)
+# print(dist_BA)
+#
+# print_format_string("DISTANCE CA", 20)
+# dist_CA, _ = calculate_causal_distance_between_images(img_C, img_A, scaling_parameter_c)
+# print(dist_CA)
+#
+#
+# print_format_string("DISTANCE DA", 20)
+# dist_DA, _ = calculate_causal_distance_between_images(img_D, img_A, scaling_parameter_c)
+# print(dist_DA)
+#
